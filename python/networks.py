7f2524976a83231450f2ede4a689b131054c1161
Patrick <patrickbryant@me.com>
2022-12-21 16:23:17 -0500

    nearly got a basic CNN training going

---
+++ python/networks.py
@@ -0,0 +1,377 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+
+def vector_print(vector, end='\n'):
+    vectorString = ", ".join([f'{element:7.2f}' for element in vector])
+    print(vectorString, end=end)
+
+    
+class Ghost_Batch_Norm(nn.Module): #https://arxiv.org/pdf/1705.08741v2.pdf has what seem like typos in GBN definition. 
+    def __init__(self, features, ghost_batch_size=32, number_of_ghost_batches=64, n_averaging=1, stride=1, eta=0.9, bias=True, device='cpu', name='', conv=False, features_out=None):
+        super(Ghost_Batch_Norm, self).__init__()
+        self.name = name
+        self.index = None
+        self.stride = stride
+        self.device = device
+        self.features = features
+        self.features_out = features_out if features_out is not None else self.features
+        self.register_buffer('ghost_batch_size', torch.tensor(ghost_batch_size, dtype=torch.long))
+        self.register_buffer('n_ghost_batches', torch.tensor(number_of_ghost_batches*n_averaging, dtype=torch.long))
+        self.conv = False
+        self.gamma = None
+        self.bias = None
+        self.updates = 0
+        if conv:
+            self.conv = nn.Conv1d(self.features, self.features_out, stride, stride=stride, bias=bias)
+        else:
+            self.gamma = nn.Parameter(torch .ones(self.features))
+            if bias:
+                self.bias = nn.Parameter(torch.zeros(self.features))
+        self.running_stats = True
+        self.initialized   = False
+
+        self.register_buffer('eps',  torch.tensor(1e-5, dtype=torch.float))
+        self.register_buffer('eta',  torch.tensor(eta, dtype=torch.float))
+        self.register_buffer('m',    torch.zeros((1,1,self.stride,self.features), dtype=torch.float))
+        self.register_buffer('s',    torch.ones ((1,1,self.stride,self.features), dtype=torch.float))
+        self.register_buffer('zero', torch.tensor(0., dtype=torch.float))
+        self.register_buffer('one',  torch.tensor(1., dtype=torch.float))
+        self.register_buffer('two',  torch.tensor(2., dtype=torch.float))
+
+    def print(self):
+        print('-'*50)
+        print(self.name)
+        for i in range(self.stride):
+            print(" mean ",end='')
+            vector_print(self.m[0,0,i,:])
+        for i in range(self.stride):
+            print("  std ", end='')
+            vector_print(self.s[0,0,i,:])
+        if self.gamma is not None:
+            print("gamma ", end='')
+            vector_print(self.gamma.data)
+            if self.bias is not None:
+                print(" bias ", end='')
+                vector_print(self.bias.data)
+        print()
+        
+    @torch.no_grad()
+    def set_mean_std(self, x, mask=None):
+        batch_size = x.shape[0]
+        pixels = x.shape[2]
+        pixel_groups = pixels//self.stride
+        x = x.detach().transpose(1,2).contiguous().view(batch_size*pixels, 1, self.features)
+        if mask is not None:
+            mask = mask.detach().view(batch_size*pixels)
+            x = x[mask==0,:,:]
+        # this won't work for any layers with stride!=1
+        x = x.view(-1, 1, self.stride, self.features)            
+        m64 = x.mean(dim=0, keepdim=True, dtype=torch.float64)#.to(self.device)
+        self.m = m64.type(torch.float32).to(self.device)
+        self.s = x .std(dim=0, keepdim=True).to(self.device)
+        self.initialized = True
+        self.running_stats = False
+        self.print()        
+
+    def set_ghost_batches(self, n_ghost_batches):
+        self.n_ghost_batches = torch.tensor(n_ghost_batches, dtype=torch.long).to(self.device)
+
+    def forward(self, x, mask=None, debug=False):
+        batch_size = x.shape[0]
+        pixels = x.shape[2]
+        pixel_groups = pixels//self.stride
+
+        if self.training and self.n_ghost_batches!=0 and not self.PCC:
+            self.ghost_batch_size = batch_size // self.n_ghost_batches.abs()
+
+            #
+            # Apply batch normalization with Ghost Batch statistics
+            #
+            x = x.transpose(1,2).contiguous().view(self.n_ghost_batches.abs(), self.ghost_batch_size*pixel_groups, self.stride, self.features)
+            
+            if mask is None:
+                gbm =  x.mean(dim=1, keepdim=True)
+                gbs = (x. var(dim=1, keepdim=True) + self.eps).sqrt()
+
+            else:
+                # Compute masked mean and std for each ghost batch
+                mask = mask.view(self.n_ghost_batches.abs(), self.ghost_batch_size*pixel_groups, self.stride, 1)
+                nUnmasked = (mask==0).sum(dim=1,keepdim=True).float()#.to(self.device)
+                unmasked0 = (nUnmasked==self.zero).float()#.to(self.device)
+                unmasked1 = (nUnmasked==self.one ).float()#.to(self.device)
+                denomMean = nUnmasked + unmasked0 # prevent divide by zero
+                denomVar  = nUnmasked + unmasked0*self.two + unmasked1 - self.one # prevent divide by zero with bessel correction
+                gbm =    x     .masked_fill(mask,0)    .sum(dim=1, keepdim=True) / denomMean
+                gbs = (((x-gbm).masked_fill(mask,0)**2).sum(dim=1, keepdim=True) / denomVar + self.eps).sqrt()
+
+            #
+            # Keep track of running mean and standard deviation. 
+            #
+            if self.running_stats or debug:
+                # Use mean over ghost batches for running mean and std
+                bm = gbm.detach().mean(dim=0, keepdim=True)
+                bs = gbs.detach().mean(dim=0, keepdim=True)
+
+                if debug and self.initialized:
+                    gbms = gbm.detach().std(dim=0, keepdim=True)
+                    gbss = gbs.detach().std(dim=0, keepdim=True)
+                    m_pulls = (bm-self.m)/gbms
+                    s_pulls = (bs-self.s)/gbss
+                    #s_ratio = bs/self.s
+                    #if (m_pulls.abs()>5).any() or (s_pulls.abs()>5).any():
+                    print()
+                    print(self.name)
+                    print('self.m\n',self.m)
+                    print('    bm\n', bm)
+                    print('  gbms\n',gbms)
+                    print('m_pulls\n',m_pulls,m_pulls.abs().mean(),m_pulls.abs().max())
+                    print('-------------------------')
+                    print('self.s\n',self.s)
+                    print('    bs\n',bs)
+                    print('  gbss\n',gbss)
+                    print('s_pulls\n',s_pulls,s_pulls.abs().mean(),s_pulls.abs().max())
+                    #print('s_ratio\n',s_ratio)
+                    print()
+                    #input()
+                    
+            if self.running_stats:
+                # Simplest possible method
+                if self.initialized:
+                    self.m = self.eta*self.m + (self.one-self.eta)*bm
+                    self.s = self.eta*self.s + (self.one-self.eta)*bs
+                else:
+                    self.m = self.zero*self.m+bm
+                    self.s = self.zero*self.s+bs
+                    self.initialized = True
+
+            if self.n_ghost_batches>0:
+                x = x - gbm
+                x = x / gbs
+            else:
+                x = x.view(batch_size, pixel_groups, self.stride, self.features)
+                x = x - self.m
+                x = x / self.s
+                
+        else:
+            # Use mean and standard deviation buffers rather than batch statistics
+            #.view(self.n_ghost_batches, self.ghost_batch_size*pixel_groups, self.stride, self.features)
+            x = x.transpose(1,2).view(batch_size, pixel_groups, self.stride, self.features)
+            x = x - self.m
+            x = x / self.s
+
+        if self.conv:
+            # back to standard indexing for convolutions: [batch, feature, pixel]
+            x = x.view(batch_size, pixels, self.features).transpose(1,2).contiguous()
+            x = self.conv(x)
+        else:
+            x = x * self.gamma
+            if self.bias is not None:
+                x = x + self.bias
+            # back to standard indexing for convolutions: [batch, feature, pixel]
+            x = x.view(batch_size, pixels, self.features).transpose(1,2).contiguous()
+        return x            
+
+    
+#
+# some basic four-vector operations
+#
+def PxPyPzE(v): # need this to be able to add four-vectors
+    pt  = v[:,0:1]
+    eta = v[:,1:2]
+    phi = v[:,2:3]
+    m   = v[:,3:4]
+    
+    Px, Py, Pz = pt*phi.cos(), pt*phi.sin(), pt*eta.sinh()
+    E = (pt**2 + Pz**2 + m**2).sqrt()
+
+    return torch.cat( (Px,Py,Pz,E), 1 )
+
+
+def PtEtaPhiM(v):
+    px = v[:,0:1]
+    py = v[:,1:2]
+    pz = v[:,2:3]
+    e  = v[:,3:4]
+
+    Pt  = (px**2+py**2).sqrt()
+    ysign = py.sign()
+    ysign = ysign + (ysign==0.0).float() # if py==0, px==Pt and acos(1)=pi/2 so we need zero protection on py.sign()
+    Phi = (px/Pt).acos() * ysign
+    Eta = (pz/Pt).asinh()
+
+    M = F.relu(e**2 - px**2 - py**2 - pz**2).sqrt()
+
+    return torch.cat( (Pt, Eta, Phi, M) , 1 ) 
+    
+
+def addFourVectors(v1, v2, v1PxPyPzE=None, v2PxPyPzE=None): # output added four-vectors
+    #vX[batch index, (pt,eta,phi,m), object index]
+
+    if v1PxPyPzE is None:
+        v1PxPyPzE = PxPyPzE(v1)
+    if v2PxPyPzE is None:
+        v2PxPyPzE = PxPyPzE(v2)
+
+    v12PxPyPzE = v1PxPyPzE + v2PxPyPzE
+    v12        = PtEtaPhiM(v12PxPyPzE)
+
+    return v12, v12PxPyPzE
+
+
+def calcDeltaPhi(v1, v2): #expects eta, phi representation
+    dPhi12 = (v1[:,2:3]-v2[:,2:3])%math.tau
+    dPhi21 = (v2[:,2:3]-v1[:,2:3])%math.tau
+    dPhi = torch.min(dPhi12,dPhi21)
+    return dPhi
+
+#
+# Some different non-linear units
+#
+def SiLU(x): #SiLU https://arxiv.org/pdf/1702.03118.pdf   Swish https://arxiv.org/pdf/1710.05941.pdf
+    return x * torch.sigmoid(x)
+
+
+def NonLU(x): #Pick the default non-Linear Unit
+    return SiLU(x) # often slightly better performance than standard ReLU
+    #return F.relu(x)
+    #return F.rrelu(x, training=training)
+    #return F.leaky_relu(x, negative_slope=0.1)
+    #return F.elu(x)
+    
+
+#
+# embed inputs in feature space
+#
+class Input_Embed(nn.Module):
+    def __init__(self, dimension, device='cpu'):
+        super(Input_Embed, self).__init__()
+        self.d = dimension
+        self.device = device
+
+        # embed inputs to dijetResNetBlock in target feature space
+        self.jet_embed     = Ghost_Batch_Norm(4, features_out=self.d, conv=True, name='jet embedder') # phi is relative to dijet
+        self.jet_conv      = Ghost_Batch_Norm(self.d, conv=True, name='jet convolution')
+
+        self.dijet_embed   = Ghost_Batch_Norm(4, features_out=self.d, conv=True, name='dijet embedder') # phi is relative to quadjet
+        self.dijet_conv    = Ghost_Batch_Norm(self.d, conv=True, name='dijet convolution') 
+
+        self.quadjet_embed = Ghost_Batch_Norm(3, features_out=self.d, conv=True, name='quadjet embedder') # phi is removed
+        self.quadjet_conv  = Ghost_Batch_Norm(self.d, conv=True, name='quadjet convolution')
+
+    def data_prep(self, j):
+        j=j.clone()# prevent overwritting data from dataloader when doing operations directly from RAM rather than copying to VRAM
+        n = j.shape[0]
+        j = j.view(n,4,4)
+
+        # make leading jet eta positive direction so detector absolute eta info is removed
+        etaSign = 1-2*(j[:,1,0:1]<0).float() # -1 if eta is negative, +1 if eta is zero or positive
+        j[:,1,:] = etaSign * j[:,1,:]
+        
+        d, dPxPyPzE = addFourVectors(j[:,:,(0,2,0,1,0,1)], 
+                                     j[:,:,(1,3,2,3,3,2)])
+
+        q, qPxPyPzE = addFourVectors(d[:,:,(0,2,4)],
+                                     d[:,:,(1,3,5)], 
+                                     v1PxPyPzE = dPxPyPzE[:,:,(0,2,4)],
+                                     v2PxPyPzE = dPxPyPzE[:,:,(1,3,5)])        
+
+        # take log of pt, mass variables which have long tails
+        j[:,(0,3),:] = torch.log(1+j[:,(0,3),:])
+        d[:,(0,3),:] = torch.log(1+d[:,(0,3),:])
+        q[:,(0,3),:] = torch.log(1+q[:,(0,3),:])
+
+        # set up all possible jet pairings
+        j = torch.cat([j, j[:,:,(0,2,1,3)], j[:,:,(0,3,1,2)]],2)
+
+        # only keep relative angular information so that learned features are invariant under global phi rotations and eta/phi flips
+        j[:,2:3,(0,2,4,6,8,10)] = calcDeltaPhi(d, j[:,:,(0,2,4,6,8,10)]) # replace jet phi with deltaPhi between dijet and jet
+        j[:,2:3,(1,3,5,7,9,11)] = calcDeltaPhi(d, j[:,:,(1,3,5,7,9,11)])
+
+        d[:,2:3,(0,2,4)] = calcDeltaPhi(q, d[:,:,(0,2,4)])
+        d[:,2:3,(1,3,4)] = calcDeltaPhi(q, d[:,:,(1,3,5)])
+
+        q = torch.cat( (q[:,:2,:],q[:,3:,:]) , 1 ) # remove phi from quadjet features
+
+        return j, d, q
+
+    def setMeanStd(self, j):
+        j, d, q = self.data_prep(j)
+
+        self    .jet_embed.set_mean_std(j)
+        self  .dijet_embed.set_mean_std(d)
+        self.quadjet_embed.set_mean_std(q)
+
+    def set_ghost_batches(self, n_ghost_batches):
+        self.    jet_embed.set_ghost_batches(n_ghost_batches)
+        self.  dijet_embed.set_ghost_batches(n_ghost_batches)
+        self.quadjet_embed.set_ghost_batches(n_ghost_batches)
+
+        self.    jet_conv.set_ghost_batches(n_ghost_batches)
+        self.  dijet_conv.set_ghost_batches(n_ghost_batches)
+        self.quadjet_conv.set_ghost_batches(n_ghost_batches)
+
+    def forward(self, j):
+        j, d, q = self.data_prep(j)
+
+        j = self    .jet_embed(j)
+        d = self  .dijet_embed(d)
+        q = self.quadjet_embed(q)
+
+        j = self    .jet_conv(NonLU(j))
+        d = self  .dijet_conv(NonLU(d))
+        q = self.quadjet_conv(NonLU(q))
+
+        return j, d, q
+
+
+class Basic_CNN(nn.Module):
+    def __init__(self, dimension, n_classes=2):
+        super(Basic_CNN, self).__init__()
+        self.d = dimension
+        self.n_classes = n_classes
+        self.n_ghost_batches = 64
+
+        self.input_embed = Input_Embed(self.d)
+
+        self.jets_to_dijets     = Ghost_Batch_Norm(self.d, stride=2, conv=True)
+        self.dijets_to_quadjets = Ghost_Batch_Norm(self.d, stride=2, conv=True)
+
+        self.select_q = Ghost_Batch_Norm(self.d, features_out=1, conv=True, bias=False)
+        self.out      = Ghost_Batch_Norm(self.d, features_out=self.n_classes, conv=True)
+
+    def set_mean_std(self, j):
+        self.input_embed.set_mean_std(j)
+
+    def set_ghost_batches(self, n_ghost_batches):
+        self.input_embed.set_ghost_batches(n_ghost_batches)
+        self.jets_to_dijets.set_ghost_batches(n_ghost_batches)
+        self.dijets_to_quadjets.set_ghost_batches(n_ghost_batches)
+        self.select_q.set_ghost_batches(n_ghost_batches)
+        self.out.set_ghost_batches(n_ghost_batches)
+        self.n_ghost_batches = n_ghost_batches
+
+    def forward(self, j):
+        j, d, q = self.input_embed(j)
+
+        d = d + NonLU(self.jets_to_dijets(j))
+        q = q + NonLU(self.dijets_to_quadjets(d))
+
+        #compute a score for each event quadjet
+        q_logits = self.select_q(q)
+
+        #convert the score to a 'probability' with softmax. This way the classifier is learning which pairing is most relevant to the classification task at hand.
+        q_score = F.softmax(q_logits, dim=-1)
+        q_logits = q_logits.view(-1, 3)
+
+        #add together the quadjets with their corresponding probability weight
+        e = torch.matmul(q, q_score.transpose(1,2))
+
+        #project the final event-level pixel into the class score space
+        c_logits = self.out(e)
+        c_logits = c_logits.view(-1, self.n_classes)
+
+        return c_logits, q_logits
+        
--- python/train.py
+++ python/train.py
@@ -1,23 +1,24 @@
 from glob import glob
+import sys
 from coffea import util
 import awkward as ak
 import numpy as np
 import torch
+import torch.optim as optim
+import torch.nn.functional as F
 from torch.utils.data import TensorDataset, DataLoader
+import networks
 
-class classInfo:
+
+class ClassInfo:
     def __init__(self, abbreviation='', name='', index=None, color=''):
         self.abbreviation = abbreviation
         self.name = name
         self.index = index
         self.color = color
 
-d4 = classInfo(abbreviation='d4', name= 'FourTag Data', color='red')
-d3 = classInfo(abbreviation='d3', name='ThreeTag Data', color='orange')
-
-#Load data
-coffea_4b = sorted(glob('data/fourTag_picoAOD*.coffea'))
-coffea_3b = sorted(glob('data/threeTag_picoAOD*.coffea'))
+d4 = ClassInfo(abbreviation='d4', name= 'FourTag Data', color='red')
+d3 = ClassInfo(abbreviation='d3', name='ThreeTag Data', color='orange')
 
 def load(cfiles):
     event = []
@@ -26,32 +27,14 @@ def load(cfiles):
         event.append(util.load(cfile))
     return ak.concatenate(event)
 
-event_3b = load(coffea_3b)
-event_4b = load(coffea_4b)
-
-event_3b['d3'] = True
-event_3b['d4'] = False
-
-event_4b['d3'] = False
-event_4b['d4'] = True
-
-event = ak.concatenate([event_3b, event_4b])
-
 classes = [d4,d3]
 for i,c in enumerate(classes): c.index=i
 
-event['class'] = d4.index*event.d4 + d3.index*event.d3 # for binary classification this seems dumb, makes sense when you have multi-class classification
-
-#Split into training and validaiton sets and format into pytorch tensors
-train_valid_modulus = 3
-train_valid_offset  = 0
-
-event['valid'] = event.event%train_valid_modulus == train_valid_offset
-event['train'] = ~event.valid
-
 # convert coffea objects in to pytorch tensors
+train_valid_modulus = 3
 def coffea_to_tensor(event):
     J = torch.FloatTensor( event['Jet',('pt','eta','phi','mass')].to_numpy().view(np.float32).reshape(-1,4,4) ) # [event,jet,feature]
+    J = J.transpose(1,2).contiguous() # [event,feature,jet]
     w = torch.FloatTensor( event['weight'].to_numpy().view(np.float32) )
     y = torch.LongTensor( np.asarray(event['class'], dtype=np.uint8) )
     R  = 1*torch.LongTensor( event['SB'].to_numpy().view(np.uint8) )
@@ -60,6 +43,123 @@ def coffea_to_tensor(event):
     dataset = TensorDataset(J, y, w, R, e)
     return dataset
 
-dataset_train = coffea_to_tensor(event[event.train])
-dataset_valid = coffea_to_tensor(event[event.valid])
 
+lr_init  = 0.01
+lr_scale = 0.25
+bs_milestones = [1,3,6,10]
+lr_milestones = [15,16,17,18,19,20,21,22,23,24]
+
+train_batch_size = 2**10
+infer_batch_size = 2**14
+max_train_batch_size = train_batch_size*64
+
+
+class Loader_Result:
+    def __init__(self, trainer, dataset, n_classes=2, train=False):
+        self.infer_loader = DataLoader(dataset=dataset, batch_size=infer_batch_size, shuffle=False, num_workers=1, pin_memory=True)
+        self.train_loader = DataLoader(dataset=dataset, batch_size=train_batch_size, shuffle=True,  num_workers=1, pin_memory=True, drop_last=True) if train else None
+        self.n = len(dataset)
+        self.w = dataset.tensors[2]
+        self.w_sum = self.w.sum()
+        self.cross_entropy = torch.zeros(self.n)
+        self.n_done = 0
+        self.loaded_die_loss = trainer.loaded_die_loss
+
+    def eval(self):
+        self.n_done = 0
+        
+    def infer_batch(self, c_logits, q_logits, y, w, R, e):
+        n_batch = c_logits.shape[0]
+        y_pred_batch = F.softmax(c_logits, dim=-1)
+        cross_entropy_batch = F.cross_entropy(c_logits, y, reduction='none')
+
+        self.cross_entropy[self.n_done:self.n_done+n_batch] = cross_entropy_batch
+
+        self.n_done += n_batch
+
+    def infer_done(self):
+        self.loss = (self.w * self.cross_entropy).sum()/self.w_sum/self.loaded_die_loss
+        self.n_done = 0
+
+
+print_step = 10
+class Trainer:
+    def __init__(self, train_valid_offset=0):
+        self.train_valid_offset = train_valid_offset
+        self.network = networks.Basic_CNN(8)
+        n_trainable_parameters = sum(p.numel() for p in self.network.parameters() if p.requires_grad)
+        print(f'Network has {n_trainable_parameters} trainable parameters')
+
+        self.optimizer = optim.Adam(self.network.parameters(), lr=lr_init, amsgrad=False)
+        self.scheduler = optim.lr_scheduler.MultiStepLR(self.optimizer, lr_milestones, gamma=lr_scale, last_epoch=-1)
+
+        
+    def make_loaders(self, event):
+        wd4 = sum(event[event.d4].weight)
+        wd3 = sum(event[event.d3].weight)
+        w   = sum(event.weight)
+        fC = torch.FloatTensor([wd4/w, wd3/w])
+        # compute the loss you would get if you only used the class fraction to predict class probability (ie an n_class sided die loaded to land with the right fraction on each class)
+        self.loaded_die_loss = -(fC*fC.log()).sum()
+
+        #Split into training and validaiton sets and format into pytorch tensors
+        valid = event.event%train_valid_modulus == self.train_valid_offset
+        train = ~valid
+
+        dataset_train = coffea_to_tensor(event[train])
+        dataset_valid = coffea_to_tensor(event[valid])
+
+        self.train_result = Loader_Result(self, dataset_train, train=True)
+        self.valid_result = Loader_Result(self, dataset_valid)
+
+        print(f'{self.train_result.n:,} training samples split into {len(self.train_result.train_loader):,} batches of {train_batch_size:,}')
+
+
+    @torch.no_grad()
+    def inference(self, result):
+        self.network.eval()
+        result.eval()
+
+        n_done = 0
+        for i, (J, y, w, R, e) in enumerate(result.infer_loader):
+            c_logits, q_logits = self.network(J)
+            result.infer_batch(c_logits, q_logits, y, w, R, e)
+
+            if int(i+1) % print_step == 0:
+                percent = float(i+1)*100/len(result.infer_loader)
+                sys.stdout.write(f'\rEvaluating {percent:3.0f}%')
+                sys.stdout.flush()
+
+        result.infer_done()
+
+    def train_inference(self):
+        self.inference(self.train_result)
+        sys.stdout.write(' '*200)
+        sys.stdout.flush()
+        print('\r',end='')
+        print(f'{self.train_result.loss}')
+
+if __name__ == '__main__':
+    #Load data
+    coffea_4b = sorted(glob('data/fourTag_picoAOD*.coffea'))
+    coffea_3b = sorted(glob('data/threeTag_picoAOD*.coffea'))
+
+    event_3b = load(coffea_3b)
+    event_4b = load(coffea_4b)
+
+    event_3b['d3'] = True
+    event_3b['d4'] = False
+
+    event_4b['d3'] = False
+    event_4b['d4'] = True
+    
+    event = ak.concatenate([event_3b, event_4b])
+
+    event['class'] = d4.index*event.d4 + d3.index*event.d3 # for binary classification this seems dumb, makes sense when you have multi-class classification
+
+    t=Trainer()
+    t.make_loaders(event)
+    t.train_inference()
+
+
+    
